738ff721594a66fe3c9ce6167912b85cf0be9114
zhoupeng <zhoupeng@xjgreat.com>
2018-10-08 09:53:28 +0800

    o

--- libsuperdog.rb
+++ libsuperdog.rb
@@ -1,8 +1,19 @@
-require 'dl'
-require 'dl/import'
+RV = Gem::Version.new(RUBY_VERSION)
+RV22 = Gem::Version.new("2.2.0")
+
+if RV >= RV22
+    require 'fiddle/import'
+else
+    require 'dl'
+    require 'dl/import'
+end
 
 module LibSuperdog
-        extend DL::Importer
+        if RV >= RV22
+            extend Fiddle::Importer
+        else
+            extend DL::Importer
+        end
         dlload './Linux/API/Licensing/C/x86_64/libdog_linux_x86_64_demo.so'
         extern 'dog_status_t DOG_CALLCONV dog_encrypt(dog_handle_t handle,void *buffer,dog_size_t length)'
         extern 'dog_status_t DOG_CALLCONV dog_decrypt(dog_handle_t handle,void *buffer,dog_size_t length);'
